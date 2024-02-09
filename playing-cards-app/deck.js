d37f8b0279ae0bcbe1e5eb62c00b10938e2f6c92
scarflox <timitimor@gmail.com>
2024-02-09 14:07:55 +0200

    notes

--- playing-cards-app/deck.js
+++ playing-cards-app/deck.js
@@ -4,37 +4,47 @@ const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', '
 let deck = null; //Preparing deck as null
 
 const initializeDeck = () => {
-  deck = [];
+  deck = []; 
   suits.forEach(suit => {
     values.forEach(value => {
       deck.push({ suit, value });
     });
   });
-};
+}; /*As the name suggests, initializing the deck, starts as an array, for each one of the suits,
+a number will be assigned "value", and then both the new suit and value are pushed into the array "deck"
+*/
 
 const getDeck = () => {
   if (!deck) {
     initializeDeck();
   }
   return deck;
-};
+}; /*A function checking if the deck is null, if it is, it calls "initializeDeck()" to make one,
+     if not, returns the current deck.
+*/
 
 exports.getDeck = getDeck;
 exports.addCard = (card) => deck.push(card);
 exports.deleteCard = (cardToDelete) => {
     // Find the index of the first card that matches the criteria
-    const index = deck.findIndex(card => card.suit === cardToDelete.suit && card.value === cardToDelete.value);
+    const index = deck.findIndex(card => card.suit === cardToDelete.suit && card.value === cardToDelete.value); 
+    // Returns -1 if a card wasn't found.
   
     // If a matching card is found (index !== -1), remove it
     if (index !== -1) {
       deck.splice(index, 1); // Remove one item at the found index
     }
+    else{
+      alert('No card was found!');
+    }
+    
+    
   };
 
   
 exports.shuffleDeck = () => {
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]];
+    [deck[i], deck[j]] = [deck[j], deck[i]]; // switches places between [deck[j], deck[i]] repeatedly during the loop.
   }
 };
--- playing-cards-app/server.js
+++ playing-cards-app/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const { getDeck, addCard, deleteCard, shuffleDeck } = require('./deck');
-const app = express();
-const port = 3000;
+const express = require('express'); // Express: web framework for Node.js
+const { getDeck, addCard, deleteCard, shuffleDeck } = require('./deck'); // Import of functions from deck.js, managing the deck of cards.
+const app = express(); // Creating the Express application
+const port = 3000; // Common port used for development purposes.
 
-app.use(express.json());
-app.use(express.static('public'));
+// Middleware:
+app.use(express.json()); // Parses incoming requests.you'
+app.use(express.static('public')); // This serves static files from the public directory.
+
+/* Middleware is a function that has access to the request object (`req`), the response object (`res`), 
+and the next middleware function in the application's request-response cycle. 
+These functions can execute any code, make changes to the request and response objects, 
+end the request-response cycle and call the next middleware function.*/
+
+
+
 
 // GET – returns all the cards in our deck
 app.get('/deck', (req, res) => {
@@ -39,7 +48,7 @@ app.patch('/deck/shuffle', (req, res) => {
 app.get('/testimage', (req, res) => {
     res.sendFile(__dirname + '/public/images/5_of_diamonds.png');
   });
-
+// Starting the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
